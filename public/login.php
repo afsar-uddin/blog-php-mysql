8743dbe5249942b624b7cad1830a4ea275f64ff3
afsar-uddin <afsarwebdev@gmail.com>
2023-08-30 06:56:43 +0600

    frontend and dashboard setup

---
+++ public/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Blog</title>
+    <link rel="stylesheet" href="../public/assets/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="nwt-login">
+        <form action="" method="post">
+            <input type="email" placeholder="Email"> <br />
+            <input type="text" placeholder="Password"> <br />
+            <input type="checkbox" id="remember">
+            <label for="remember">Remember Me</label> <br />
+            <input type="submit" value="Login">
+
+            <p>Don't have an account? <a href="signup.php">Login Here</a></p>
+        </form>
+    </div>
+    <script src="../public/assets/js/jquery.min.js"></script>
+    <script src="../public/assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ public/signup.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login | Blog</title>
+    <link rel="stylesheet" href="../public/assets/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="nwt-login">
+        <form action="" method="post">
+            <input type="text" placeholder="Username"> <br />
+            <input type="email" placeholder="Email"> <br />
+            <input type="text" placeholder="Password"> <br />
+            <input type="text" placeholder="Confirm Password"> <br />
+            <input type="checkbox" id="remember">
+            <label for="remember">Accept terms and conditions.</label> <br />
+            <input type="submit" value="Signup">
+
+            <p>Already have an account? <a href="login.php">Sign Here</a></p>
+        </form>
+    </div>
+
+    <script src="../public/assets/js/jquery.min.js"></script>
+    <script src="../public/assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
